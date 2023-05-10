01d99520e4971ad36f5d8de18bdddbf496722f59
tolgazorlu <tolgazorlu17@gmail.com>
2023-05-10 15:07:37 +0300

    project uploaded

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
+app.get('/', (req, res) => {
+    res.send(blog)
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda çalışıyor...`)
+})
