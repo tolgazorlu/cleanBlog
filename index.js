86704f25da1a3f2dd338d41503aa26341782ba37
tolgazorlu <tolgazorlu17@gmail.com>
2023-05-11 12:59:27 +0300

    template engine used

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
+//ROUTES
 app.get('/', (req, res) => {
-    res.send(blog)
-})
+  res.render('index');
+});
 
-const port = 3000;
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const port = 3001;
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda çalışıyor...`)
-})
+  console.log(`Sunucu ${port} portunda çalışıyor...`);
+});
