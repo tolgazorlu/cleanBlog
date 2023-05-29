d26a63d6b3f799d82126833ad0a597cacb2461ee
Tolga Zorlu <44470351+tolgazorlu@users.noreply.github.com>
2023-05-29 19:24:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb+srv://tolgazorlu17:gA2YAyU1A7JulzMp@cluster0.vtnj3mg.mongodb.net/?retryWrites=true&w=majority',
+    '',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
